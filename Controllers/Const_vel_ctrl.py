188e5a8cb56cdfa5c7d5358acf49f2f6f888795d
martinjensen37 <martin@AAU122002.id.aau.dk>
2020-11-10 10:43:18 +0100

    Changed the topic to /mir_direction and modified the remap function.

--- Controllers/Const_vel_ctrl.py
+++ Controllers/Const_vel_ctrl.py
@@ -2,25 +2,25 @@ import rospy
 import numpy as np
 from geometry_msgs.msg import Pose
 from nav_msgs.msg import Odometry
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, Int32
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from geometry_msgs.msg import TwistStamped
 import math
 
 yaw_current = 0.0
 roll_voice = pitch_voice = yaw_voice = 0.0
-const_vel = 0.3
+const_vel = 0.2
 
 kp=0.1
 
 def remap(angle):
 
     if angle < 0:
-        remap_angle = abs(angle) + 180
+        remap_angle = abs(angle+180) + 180
     else:
         remap_angle = angle
 
-    return angle #remap_angle
+    return remap_angle #remap_angle
 
 
 def get_rotation(msg):
@@ -40,7 +40,7 @@ def get_direction(msg1):
 # Start up program
 rospy.init_node('Voice_ctrl')
 sub = rospy.Subscriber('/odom', Odometry, get_rotation)
-sub1 = rospy.Subscriber('/voice_direction', Float32, get_direction)
+sub1 = rospy.Subscriber('/mir_direction', Int32, get_direction)
 pub = rospy.Publisher('cmd_vel', TwistStamped, queue_size = 1)
 r = rospy.Rate(10)
 command = TwistStamped()
@@ -62,12 +62,12 @@ while not rospy.is_shutdown():
         else:
             sign = 1
     # calculate the true error between [0, 180] for the angle to overcome wrap around
-    phi = math.abs(yaw_voice - yaw_current) % 360
-    dist = 360 - phi if phi > 180 else dist = phi
+    phi = abs(yaw_voice - yaw_current) % 360
+    dist = 360 - phi if phi > 180 else phi
     print('This is the difference: ', dist)
     print('Goal_pose:{0} Current_pose:{1}'.format(yaw_voice, yaw_current))
     # add dead zone, to avoid oscillations
-    if dist > 2: # can be made with degrees
+    if dist > 5: # can be made with degrees
         command.twist.angular.z = sign*const_vel
         pub.publish(command)
         print('Sending ctrl commands now!')
